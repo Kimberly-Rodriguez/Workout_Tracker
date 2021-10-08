5e1241530ae9a8c2660b3c1aef1b249b21876d37
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-10-08 13:20:22 -0700

    ger route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const Workout = require('./models/Workout');
 
 
-// http://localhost:5001/api/workout
+// http://localhost:5002/api/workout
 router.get('/api/workout', (req, res) => {
 Workout.aggregate([
   {$addFields: {totalDuration: { $sum: "$exercises.duration"} } },
@@ -13,6 +13,19 @@ Workout.aggregate([
 .catch(err => {
   res.json(err)
 });
+});
+
+// http://localhost:5002/api/workout/(id/number)
+router.put('/api/workout/:id', ({body, params}, res) => {
+  Workout.findByIdAndUpdate(params.id, {
+    $push: {exercises: body}
+    })
+    .then(userData => {
+      res.json(userData);
+    })
+    .catch(err => {
+      res.json(err)
+    });
 });
 
 
